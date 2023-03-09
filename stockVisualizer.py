0f6be3204673af92542c83c4fb8785c0a2b4afed
katie026 <>
2023-03-09 15:18:37 -0600

    defined the functions

--- stockVisualizer.py
+++ stockVisualizer.py
@@ -1,1 +1,31 @@
+# API stuff
 
+
+
+# Write our functions
+def getSymbol():
+    return
+
+def getChartType():
+    return
+
+def getTimeSeries():
+    return
+
+def getBeginDate():
+    return
+
+def getEndDate():
+    return
+
+def generateGraph():
+    return
+
+
+# Call our functions
+getSymbol()
+getChartType()
+getTimeSeries()
+getBeginDate()
+getEndDate()
+generateGraph()
