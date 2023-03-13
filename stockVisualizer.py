c4b9f99a8c0214c5958e06344e6bb9a7a1c92aea
katie026 <>
2023-03-12 19:12:54 -0500

    added varibles to main function

--- stockVisualizer.py
+++ stockVisualizer.py
@@ -43,9 +43,9 @@ def getDate(dateType):
             continue
     return date
 
-def generateGraph(symbol, chart, timeSeries, beginDate, endDate):
-    inputdata = [symbol, chart, timeSeries, beginDate, endDate]
-    if (inputdata[2] == 'INTRADAY'):
+def generateGraph(symbol, chart, timeSeries, startDate, endDate):
+    inputData = [symbol, chart, timeSeries, startDate, endDate]
+    if (inputData[2] == 'INTRADAY'):
         # this will depend upon the user's date choice. unfortunately video does not explain this so we will have to ask him if there is a defaul intraday interval or if not. if there is not, user will have to be able to choose this as well.
         # if does have intraday, will have to add this to data variable as part of api connection
         pass
@@ -59,7 +59,7 @@ def generateGraph(symbol, chart, timeSeries, beginDate, endDate):
     # you would write a function like match data where begin date is x and end date is y and then put that into array
         # you will have to figure out way to parse thru json file. this is likely where you would use postman for help and you would use json and you would parse thru this by figuring out how to select specific date
 
-    if (inputdata[1] == 'line'):
+    if (inputData[1] == 'line'):
         # USE PYGAL HERE, would likely create type_chart.add loop for each split in time series (ex. you requested 30 days of daily information, you must split this daily into different adds)
         pass
     else:
@@ -75,11 +75,11 @@ def main():
         print("-------------------------\n")
         
         # run program
-        getSymbol()
-        getChartType()
-        getTimeSeries()
+        symbol = getSymbol()
+        chart = getChartType()
+        timeSeries = getTimeSeries()
         startDate, endDate = getDateRange()
-        # generateGraph()
+        generateGraph(symbol, chart, timeSeries, startDate, endDate)
         
         # run again?
         run_again_input = input("\nWould you like to view more stock data? (y/n): ")
