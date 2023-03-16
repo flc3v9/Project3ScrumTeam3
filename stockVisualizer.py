5d1108d0f9da94180e50d85189ce07d821269469
katie026 <>
2023-03-16 14:11:24 -0500

    added test code to stockVisualizer.py

--- stockVisualizer.py
+++ stockVisualizer.py
@@ -2,19 +2,49 @@
 import pygal, lxml, requests
 from datetime import datetime
 
-
-# API
-    # for these, you will want to use the link format. we might have to see how to connect these
-
-# Write our functions
 def getSymbol():
-    return
+    #QY73AL7RJZDQESXX
+    symbol = input("Enter the stock symbol for the company you want data for: ")
+    #if(symbol != stock api data)
+    #{print("Please put in a valid stock ID")}
+    #else{return symbol}
+    return symbol
 
 def getChartType():
-    return
+    print('Chart Type\n------------------------------------')
+    print('1. Bar\n2. Line')
+    choice = ""
+    while True:
+        chartTypeOption = int(input('Enter chart type you want (1, 2): '))
+        if (chartTypeOption == 1):
+            choice = "Line"
+            return choice
+        elif (chartTypeOption == 2):
+            choice = "Bar"
+            return choice
+        else:
+            print('You have not chosen a valid option. Please only enter values 1 or 2.')
 
 def getTimeSeries():
-    return
+    print('Select the Time Series of the chart you want to Generate:\n------------------------------------')
+    print('1. Intraday\n2. Daily\n3. Weekly\n4.Monthly')
+    choice = ""
+    while True:
+        timeSeriesOption = int(input('Enter time series option(1, 2, 3, 4): '))
+        if (timeSeriesOption == 1):
+            choice = "TIME_SERIES_INTRADAY"
+            return choice
+        elif (timeSeriesOption == 2):
+            choice = "TIME_SERIES_DAILY_ADJUSTED"
+            return choice
+        elif (timeSeriesOption == 3):
+            choice = "TIME_SERIES_WEEKLY"
+            return choice
+        elif (timeSeriesOption == 4):
+            choice = "TIME_SERIES_MONTHLY"
+            return choice
+        else:
+            print('You have not chosen a valid option. Please only enter values between 1-4.')
 
 def getDateRange():
     while True:
@@ -43,51 +73,129 @@ def getDate(dateType):
             continue
     return date
 
-def generateGraph(symbol, chart, timeSeries, startDate, endDate):
-    inputData = [symbol, chart, timeSeries, startDate, endDate]
-    if (inputData[2] == 'INTRADAY'):
-        # this will depend upon the user's date choice. unfortunately video does not explain this so we will have to ask him if there is a defaul intraday interval or if not. if there is not, user will have to be able to choose this as well.
-        # if does have intraday, will have to add this to data variable as part of api connection
-        pass
-    # this is gonna be the biggest challenge as it will require us to convert the beginning and end date input, split this accordingly by time series, and then plot all of the split data in to a graph. I would highly recommend a list/array of some sort to store the data and then put that into the graph
+def getData(timeSeries, symbol):
+    # populate the query parameters
+    queryData = {'function': '', 'symbol': '', 'interval': '', 'apikey': ''}
+    queryData['function'] = timeSeries
+    queryData['symbol'] = symbol
+    queryData['apikey'] = "QY73AL7RJZDQESXX"
+    if (timeSeries == "TIME_SERIES_INTRADAY"):
+        queryData['interval'] = interval
 
-    # will likely use requests here like as shows
-    data = {'FUNCTION':'inputfromtimeseries', 'SYMBOL':'inputfromsybolfunction', 'APIKEY':'QY73AL7RJZDQESXX'}
-    requestedData = requests.get('https://www.alphavantage.co/query?',params=data)
-    # then you would use requestedData and parse thru the dates you specified in date functions; THIS IS WHAT WILL BE PUT INTO THE GRAPH
+    # request the data from alpha vantage
+    alphavantageRequest = requests.get('https://www.alphavantage.co/query', params=queryData)
+    print(alphavantageRequest.url)
 
-    # you would write a function like match data where begin date is x and end date is y and then put that into array
-        # you will have to figure out way to parse thru json file. this is likely where you would use postman for help and you would use json and you would parse thru this by figuring out how to select specific date
-
-    if (inputData[1] == 'line'):
-        # USE PYGAL HERE, would likely create type_chart.add loop for each split in time series (ex. you requested 30 days of daily information, you must split this daily into different adds)
-        pass
+    # check if request successfull
+    if alphavantageRequest.status_code == 200:
+        # turn Response object into a python dictionary
+        stocksDictionary = alphavantageRequest.json()
+        return stocksDictionary
     else:
-        # USE PYGAL BAR HERE
-        pass
+        print("Error: Request failed")
 
-    return
+def generateGraph(timeSeries, chart, stocksDictionary, startDate, endDate):
+    # get the name of the data for that time series ex. 'Monthly Time Series'
+    timeSeriesName = list(stocksDictionary)[1]
 
+    # create a reference to the time series data
+    timeSeriesData = stocksDictionary[timeSeriesName]
+
+    # name graph
+    displayStartDate = startDate.strftime("%Y/%m/%d")
+    displayEndDate = endDate.strftime("%Y/%m/%d")
+    graphTitle = f"Stock Data for {symbol}: {displayStartDate} to {displayEndDate}"
+
+    # create graph based on selected chart
+    if (chart == "Line"):
+        generateLineGraph(timeSeries, timeSeriesData, startDate, endDate, graphTitle)
+    if (chart =="Bar"):
+        generateBarChart(timeSeries, timeSeriesData, startDate, endDate, graphTitle)
+
+def generateLineGraph(timeSeries, timeSeriesData, startDate, endDate, graphTitle):
+    # list the graph lines
+    graphLines = ["1. open", "2. high", "3. low", "4. close"]
+
+    # creating line graph
+    lineGraph = pygal.Line(title=graphTitle, x_label_rotation=45, x_value_formatter=lambda dt: dt.strftime('%Y-%m-%d %H:%M:%S'))
+
+    # loop through each line
+    for line in graphLines:
+        # create list of data points for the line
+        dataPoints = list()
+        x_labels = list()
+        # loop through the timeSeriesData (objects) to populate dataPoints list
+        for date, values in timeSeriesData.items():
+            # turn the date string into a datetime object
+            if (timeSeries == "TIME_SERIES_INTRADAY"):
+                date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+            else:
+                date = datetime.strptime(date, "%Y-%m-%d")
+            # find objects based on date range
+            if date > startDate and date < endDate:
+                # assign x-coordinate the date
+                x = float(date.timestamp())
+                x_labels.append(date)
+                # assign y-coordinate it's value based on the current key (line name)
+                y = float(values[line])
+                # add the y-coordinate to the list of data points
+                dataPoints.append(y)
+        # add the line with all of the data points
+        lineGraph.add(line, dataPoints)
+    
+    # set x-axis labels
+    lineGraph.x_labels = x_labels
+
+    # open graph in browser
+    lineGraph.render_in_browser()
+
+def generateBarChart(timeSeries, timeSeriesData, startDate, endDate, graphTitle):
+    # list the chart labels
+    chartLabels = ["1. open", "2. high", "3. low", "4. close"]
+
+    # creating bar chart
+    barChart = pygal.Bar(title=graphTitle, x_label_rotation=45)
+
+    # loop through each label
+    for label in chartLabels:
+        x_labels = list() # dates in the range
+        dataPoints = list() # values per label (date is determined by x-labels)
+        # loop through the timeSeriesData (objects) to popoulate dataPoints list
+        for date, values in timeSeriesData.items():
+            # turn the date string into a datetime object
+            if (timeSeries == "TIME_SERIES_INTRADAY"):
+                date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+            else:
+                date = datetime.strptime(date, "%Y-%m-%d")
+            # find objects based on date range
+            if date > startDate and date < endDate:
+                # add date to x-axis label list
+                if (timeSeries == "TIME_SERIES_INTRADAY"):
+                    x_labels.append(date.strftime("%Y/%m/%d %H:%M:%S"))
+                else:
+                    x_labels.append(date.strftime("%Y/%m/%d"))
+                # add value to data point list
+                y = float(values[label])
+                dataPoints.append(y)
+        # add data to chart
+        barChart.add(label, dataPoints)
+    
+    # set x-axis labels
+    barChart.x_labels = x_labels
+    
+    # open graph in browser
+    barChart.render_in_browser()
+
+# Call our functions
 def main():
-    while True:
-        # title
-        print("Stock Data Visualizer")
-        print("-------------------------\n")
-        
-        # run program
+    try:
         symbol = getSymbol()
         chart = getChartType()
         timeSeries = getTimeSeries()
         startDate, endDate = getDateRange()
-        generateGraph(symbol, chart, timeSeries, startDate, endDate)
-        
-        # run again?
-        run_again_input = input("\nWould you like to view more stock data? (y/n): ")
-        if (run_again_input == "y"):
-            print("\n")
-            continue
-        else:
-            print("\nOkay, thank you for using this program.")
-            break
+        stocksDictionary = getData(timeSeries, symbol)
+        generateGraph(timeSeries, chart, stocksDictionary, startDate, endDate)
+    except Exception as error:
+        print(f"Something went wrong with the symbol. {str(error)}")
 
 main()
